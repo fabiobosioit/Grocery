f86b2cbd7d2bae2ab10c8ba651e08b758425baba
Fabio Bosio <fabio.bosio.it@gmail.com>
2022-07-17 22:16:35 +0200

    Generic Grid part 1 : introduced Generic Column and Value

--- Grocery/Shared/WeatherForecastListItem.cs
+++ Grocery/Shared/WeatherForecastListItem.cs
@@ -5,8 +5,14 @@ namespace Grocery.Shared
     public class WeatherForecastListItem
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")]
         public DateTime Date { get; set; }
+
+        [Display(Name ="Temp (C)")]
         public int TemperatureC { get; set; }
+
+        [Display(Name ="Temp (F)")]
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
 }
